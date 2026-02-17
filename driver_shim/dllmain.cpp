mbucchia/Pimax-EyeTracker-SteamVR

Initial commit.

driver_shim/dllmain.cpp
@@ -1,0 +1,45 @@
+// MIT License
+//
+// Copyright(c) 2025 Matthieu Bucchianeri
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "pch.h"
+
+#include "Tracing.h"
+
+// {15d4b714-f01f-4f5b-9a76-de69f386ade9}
+TRACELOGGING_DEFINE_PROVIDER(TraceProvider,
+                             "OpenVRDriver",
+                             (0x15d4b714, 0xf01f, 0x4f5b, 0x9a, 0x76, 0xde, 0x69, 0xf3, 0x86, 0xad, 0xe9));
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        TraceLoggingRegister(TraceProvider);
+        break;
+    case DLL_PROCESS_DETACH:
+        TraceLoggingUnregister(TraceProvider);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+        break;
+    }
+    return TRUE;
+}

mbucchia/SteamVR-Distortion-Playground

driver_shim/dllmain.cpp
@@ -1,0 +1,46 @@
+// MIT License
+//
+// Copyright(c) 2025 Matthieu Bucchianeri
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "pch.h"
+
+#include "Tracing.h"
+
+// {15d4b714-f01f-4f5b-9a76-de69f386adea}
+TRACELOGGING_DEFINE_PROVIDER(TraceProvider,
+                             "OpenVRDriver",
+                             (0x15d4b714, 0xf01f, 0x4f5b, 0x9a, 0x76, 0xde, 0x69, 0xf3, 0x86, 0xad, 0xea));
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        TraceLoggingRegister(TraceProvider);
+        TraceLoggingWrite(TraceProvider, "Hello");
+        break;
+    case DLL_PROCESS_DETACH:
+        TraceLoggingUnregister(TraceProvider);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+        break;
+    }
+    return TRUE;
+}